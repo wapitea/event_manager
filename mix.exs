a38425d81520d7e6a1ae0002a0ed920bea26d8e4
Alexandre Lepretre <alexandre.lepretre@wapitea.io>
2020-09-30 18:31:00 -0400

    Create new release : fix docs typo

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EventManager.MixProject do
   def project do
     [
       app: :event_manager,
-      version: "0.2.0",
+      version: "0.2.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
